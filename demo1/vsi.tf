553ec7933ae8411a70cab7b4ac8cc10ae143256b
Steve Lievens <steve.lievens@be.ibm.com>
2022-03-15 12:39:18 +0100

    updated variables

--- demo1/vsi.tf
+++ demo1/vsi.tf
@@ -8,8 +8,8 @@ data "ibm_is_ssh_key" "vpc_ssh_key" {
 resource "ibm_is_instance" "vsi_instance" {
   count   = 3
   name    = "${var.prefix}-instance-${count.index + 1}"
-  image   = "r010-eaebade7-d050-4e4d-b552-c45e53c9a893"
-  profile = "cx2-2x4"
+  image   = var.vpc_vsi_image
+  profile = var.vpc_vsi_profile
 
   primary_network_interface {
     subnet          = element(ibm_is_subnet.subnet.*.id, count.index)
